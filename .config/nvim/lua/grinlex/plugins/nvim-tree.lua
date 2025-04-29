5669a3496f8ea3542100b35ff1d978b3929425e8
Max <aaalllooollk@gmail.com>
2025-04-29 18:25:56 +0300

    updated nvim tree

--- .config/nvim/lua/grinlex/plugins/nvim-tree.lua
+++ .config/nvim/lua/grinlex/plugins/nvim-tree.lua
@@ -38,6 +38,12 @@ return {
             git = {
                 ignore = false,
             },
+            sync_root_with_cwd = true,
+            respect_buf_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = true,
+            },
         })
 
         local keymap = vim.keymap
