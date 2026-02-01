da23e19726555b83f583bdf0d2332826c168c835
Max <aaalllooollk@gmail.com>
2026-02-01 13:30:53 +0300

    Remove powershell ln

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,6 @@
 $_MINECRAFT_SERVER_DIR = $env:MINECRAFT_SERVER_DIR ?? "."
 
 sal ll ls
-function sli { New-Item -ItemType SymbolicLink -Path $args[1] -Target $args[0] }
 
 "start","stop","log","rcon" | ForEach-Object {
     $p = "$_MINECRAFT_SERVER_DIR/$_.ps1"
@@ -9,5 +8,3 @@ function sli { New-Item -ItemType SymbolicLink -Path $args[1] -Target $args[0] }
     Remove-Variable p
 }
 Remove-Variable _MINECRAFT_SERVER_DIR
-
-
