106c6293df4d3c973ed51597065c59c4ffa5aa12
Max <aaalllooollk@gmail.com>
2026-02-16 19:24:12 +0300

    Add dap

--- .config/nvim/lua/plugins/haunt.lua
+++ .config/nvim/lua/plugins/haunt.lua
@@ -1,8 +1,10 @@
 return {
     "adigitoleo/haunt.nvim",
+    ---@module "haunt"
+    ---@type Haunt.config
     opts = { define_commands = false },
     keys = {
-        { "<A-i>", function() require("haunt").term() end, mode = { "n" }, },
+        { "<A-i>", function() require("haunt").term({}) end, mode = { "n" }, },
         { "<A-i>", function() require("haunt").reset() end, mode = { "t" }, },
     },
 }
---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,7 @@
+return {
+    "mfussenegger/nvim-dap",
+    keys = {
+        { "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Add breakpoint at line (nvim-dap)" },
+        { "<leader>dr", "<cmd>DapContinue<cr>", desc = "Start or continue debugger (nvim-dap)" },
+    }
+}
