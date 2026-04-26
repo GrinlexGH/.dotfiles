f60b7627462b75e06e1946d706a75e40f8befc11
Max <aaalllooollk@gmail.com>
2026-04-26 13:48:47 +0000

    Add kali branch

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,5 @@
-$_MINECRAFT_SERVER_DIR = $env:MINECRAFT_SERVER_DIR ?? "."
-
 sal ll ls
 
 if ($null -ne (Get-Command zoxide -ErrorAction SilentlyContinue)) {
     zoxide init powershell | Out-String | Invoke-Expression
 }
-
-"start","stop","log","rcon" | ForEach-Object {
-    $p = "$_MINECRAFT_SERVER_DIR/$_.ps1"
-    if (Test-Path $p) { Set-Alias "mc-$_" $p }
-    Remove-Variable p
-}
-Remove-Variable _MINECRAFT_SERVER_DIR
