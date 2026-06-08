a1e57e9209e6d65c8209cb883e28a53faf3c9406
Max <aaalllooollk@gmail.com>
2026-06-08 19:41:10 +0300

    Change enter to tab

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -5,7 +5,7 @@ return {
     ---@type blink.cmp.Config
     opts = {
         keymap = {
-            preset = "enter",
+            preset = "super-tab",
             ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
             ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
 
